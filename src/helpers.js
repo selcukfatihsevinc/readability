b41502745c9ad1eda9bbd20acc3b51fd7270cd0a
Selçuk Fatih Sevinç <airsfs@gmail.com>
2019-08-27 15:00:58 +0300

    * allow comment class (highlight)

--- src/helpers.js
+++ src/helpers.js
@@ -2,10 +2,10 @@ var url = require("url");
 
 // All of the regular expressions in use within readability.
 var regexps = {
-  unlikelyCandidatesRe: /combx|modal|comment|disqus|foot|header|menu|meta|nav|rss|shoutbox|sidebar|sponsor|social|teaserlist|time|tweet|twitter/i,
+  unlikelyCandidatesRe: /combx|modal|disqus|foot|header|menu|meta|nav|rss|shoutbox|sidebar|sponsor|social|teaserlist|time|tweet|twitter/i,
   okMaybeItsACandidateRe: /and|article|body|column|main|story|entry|^post/im,
   positiveRe: /article|body|content|entry|hentry|page|pagination|post|section|chapter|description|main|blog|text/i,
-  negativeRe: /combx|comment|contact|foot|footer|footnote|link|media|meta|promo|related|scroll|shoutbox|sponsor|utility|tags|widget/i,
+  negativeRe: /combx|contact|foot|footer|footnote|link|media|meta|promo|related|scroll|shoutbox|sponsor|utility|tags|widget/i,
   divToPElementsRe: /<(a|blockquote|dl|div|img|ol|p|pre|table|ul)/i,
   replaceBrsRe: /(<br[^>]*>[ \n\r\t]*){2,}/gi,
   replaceFontsRe: /<(\/?)font[^>]*>/gi,
@@ -65,7 +65,7 @@ var prepDocument = module.exports.prepDocument = function(document) {
       }
     }
   }
-  
+
   // Strip out all <script> tags, as they *should* be useless
   var scripts = document.getElementsByTagName('script');
   [].forEach.call(scripts, function (node) {
